1dc44d41fec4c1e21d8a8c16b265515ad38039a8
yathishpoojary <yathishpoojary023@gmail.com>
2026-06-15 23:49:41 +0530

    test commit

--- src/main/java/com/learn/user_service/controller/UserController.java
+++ src/main/java/com/learn/user_service/controller/UserController.java
@@ -15,6 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/new-users")
 public class UserController {
+    
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
